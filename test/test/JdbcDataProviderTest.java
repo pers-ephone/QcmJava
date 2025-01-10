1483879d7dcbbd9320d7b0e05e7dd886ea30106b
pers-ephone <148533426+pers-ephone@users.noreply.github.com>
2025-01-10 16:32:21 +0100

    update

---
+++ test/test/JdbcDataProviderTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JdbcDataProviderTest {
+
+    @Test
+    void getRandomQuestion() {
+    }
+}
